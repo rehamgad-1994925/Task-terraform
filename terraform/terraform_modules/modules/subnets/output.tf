ab53342d9cde04c7cc8b329d090ec4242c6ead99
rehamgad-1994925 <69773699+rehamgad-1994925@users.noreply.github.com>
2021-08-06 16:41:11 +0200

    Add files via upload

---
+++ terraform/terraform_modules/modules/subnets/output.tf
@@ -0,0 +1,41 @@
+output "public1_subnet" {
+    
+    value = aws_subnet.public1.id
+}
+
+output "public2_subnet" {
+    
+    value = aws_subnet.public2.id
+}
+
+output "private1_subnet" {
+    
+    value = aws_subnet.private1.id
+}
+
+output "private2_subnet" {
+    
+    value = aws_subnet.private2.id
+}
+
+
+output "private3_subnet" {
+    
+    value = aws_subnet.private3.id
+}
+
+
+output "private4_subnet" {
+    
+    value = aws_subnet.private4.id
+}
+
+output "private5_subnet" {
+    
+    value = aws_subnet.private5.id
+}
+
+output "private6_subnet" {
+    
+    value = aws_subnet.private6.id
+}
