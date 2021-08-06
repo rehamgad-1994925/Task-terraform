595c11339eea9c8b25fc7ba1bfccf874e516e77d
rehamgad-1994925 <69773699+rehamgad-1994925@users.noreply.github.com>
2021-08-06 16:39:15 +0200

    Add files via upload

---
+++ terraform/terraform_modules/modules/routingtable/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_id"{}
+variable "environment"{}
+/*
+variable "subnet_id"{}
+variable "subnets"{}
+
+*/
+
+variable "public1"{}
+variable "public2"{}
+variable "private1"{}
+variable "private2"{}
+variable "private3"{}
+variable "private4"{}
+variable "private5"{}
+variable "private6"{}
